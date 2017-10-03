2521c99828e229e1dacb6d3c3e510f7eb4398805
nchenevey <nchenevey1@mail.csuchico.edu>
2017-10-02 17:47:58 -0700

    Empty file soon to be filled with code

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// Author: Nicholas Chenevey
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "bitmap.h"
+#include <fstream>
+
+ using namespace std;
+
+int main()
+{
+// So far this is an empty file im going to use for the project. 
+// I will create a great program that does exactly what it is supposed to
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+
+ }
