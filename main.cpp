e922a2291d16b72c6a6ccdd214dd66f779e521db
nchenevey <nchenevey1@mail.csuchico.edu>
2017-10-04 18:00:38 -0700

    Tells the pixel height and width of a bmp, expresses error if no height or width.

--- main.cpp
+++ main.cpp
@@ -4,18 +4,30 @@
 #include <string>
 #include "bitmap.h"
 #include <fstream>
-
+#include <vector>
  using namespace std;
 
 int main()
 {
 // So far this is an empty file im going to use for the project. 
 // I will create a great program that does exactly what it is supposed to
+Bitmap image;
+vector <vector <Pixel> > bmp;
+Pixel rgb;
+
+image.open("machupicchu.bmp");
+bmp = image.toPixelMatrix();
 
 
 
-
-
+if( bmp[0].size() > 0 && bmp.size() > 0 )
+{
+        cout<< "It is "<< bmp[0].size() << " pixels wide and " << bmp.size() << " pixels high."<<endl;
+}
+else
+{
+    cout<< "It would appear this has either no height or no width. Please try again." << endl;
+}
 
 
 
