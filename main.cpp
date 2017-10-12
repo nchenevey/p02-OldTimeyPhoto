3fda7256da611e0aaf1c5db98f2218e9e92301e6
nchenevey <nchenevey1@mail.csuchico.edu>
2017-10-11 18:25:34 -0700

    Done? Has a table of options

--- main.cpp
+++ main.cpp
@@ -15,37 +15,74 @@ Bitmap image;
 vector <vector <Pixel> > bmp;
 Pixel rgb;
 
-image.open("machupicchu.bmp");
-bmp = image.toPixelMatrix();
 
 
 
 int gray;
+string inPic;
 
-if( bmp[0].size() > 0 && bmp.size() > 0 )
+cout<<"Please provide the file to be modified."<<endl;
+cin>>inPic;
+
+image.open(inPic);
+
+bool validBmp = image.isImage();
+
+while(validBmp == false)
 {
-        cout<< "It is "<< bmp[0].size() << " pixels wide and " << bmp.size() << " pixels high."<<endl;
-        for(int row = 0; row < bmp.size(); row++)
+    cout<<"The file must be a 24 bit depth Windows BMP file. Please try again."<<endl;
+    cin>>inPic;
+    image.open(inPic);
+    validBmp = image.isImage();
+}
+
+
+bmp = image.toPixelMatrix();
+
+char choice;
+do
+{
+    
+    cout<<"Please select an option."<<endl;
+    cout<<"Pixel Count(P)"<<endl<<"Change color to black and white(C)"<<endl<<"Exit(E)"<<endl;
+    cin>> choice;
+
+
+    
+    if( bmp[0].size() > 0 && bmp.size() > 0 )
+    {
+        if(choice == 'p' || choice == 'P')
         {
-            for(int col = 0; col < bmp[0].size(); col++)
-            {
-                rgb = bmp[row][col];
-                gray = (rgb.red + rgb.green + rgb.blue)/3;
-                rgb.red = gray;
-                rgb.green = gray;
-                rgb.blue = gray;
-                bmp[row][col] = rgb;
-            }
+        cout<< "It is "<< bmp[0].size() << " pixels wide and " << bmp.size() << " pixels high."<<endl;
         }
-    image.fromPixelMatrix(bmp);
-    image.save("oldtimey.bmp");
-}
-else
-{
-    cout<< "It would appear this has either no height or no width. Please try again." << endl;
+        else if(choice == 'c' || choice == 'C')
+        {
+            for(int row = 0; row < bmp.size(); row++)
+            {
+                for(int col = 0; col < bmp[0].size(); col++)
+                {
+                    rgb = bmp[row][col];
+                    int r = rgb.red;
+                    int g = rgb.green;
+                    int b = rgb.blue;
+                    gray = (r + g + b)/3;
+                    rgb.red = gray;
+                    rgb.green = gray;
+                    rgb.blue = gray;
+                    bmp[row][col] = rgb;
+                }
+            }
+        image.fromPixelMatrix(bmp);
+        image.save("oldtimey.bmp");
+        }
+    }
+    else
+    {
+        cout<< "It would appear this has either no height or no width. Please try again." << endl;
  
-}
+    }
 
+}while(choice != 'E' && choice != 'e' );
 
 
 
